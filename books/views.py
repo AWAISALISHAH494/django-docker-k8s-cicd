f3f3935756100708f412ae869117158c2cde1c42
AWAISALISHAH494 <awaisalishah02@gmail.com>
2026-08-11 17:45:19 +0500

    WEBAPP LOGIC - Models - Urls - Views

--- books/views.py
+++ books/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Book
+from .forms import BookForm
 
-# Create your views here.
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'books/list.html', {'books': books})
+
+def book_create(request):
+    form = BookForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('book_list')
+    return render(request, 'books/form.html', {'form': form, 'title': 'Add Book'})
+
+def book_update(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    form = BookForm(request.POST or None, instance=book)
+    if form.is_valid():
+        form.save()
+        return redirect('book_list')
+    return render(request, 'books/form.html', {'form': form, 'title': 'Edit Book'})
+
+def book_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'books/confirm_delete.html', {'book': book})
