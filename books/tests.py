52c199236acc9036e95782dd6b34e8a216636aed
AWAISALISHAH494 <awaisalishah02@gmail.com>
2026-08-11 22:59:38 +0500

    Test added

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Book
 
-# Create your tests here.
+
+class BookModelTest(TestCase):
+
+    def setUp(self):
+        self.book = Book.objects.create(
+            title="Django for Beginners",
+            author="William",
+            published_year=2025
+        )
+
+    def test_book_creation(self):
+        self.assertEqual(self.book.title, "Django for Beginners")
+        self.assertEqual(self.book.author, "William")
+        self.assertEqual(self.book.published_year, 2025)
+
+    def test_book_string_representation(self):
+        self.assertEqual(str(self.book), "Django for Beginners")
